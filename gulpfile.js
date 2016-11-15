8ba88bb02949fc8139a42ade87eafd6ed35d1120
Maxim Korovin <Maxim Korovin>
2016-11-15 20:08:33 +0300

    first

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var server = require('gulp-server-livereload');
+var sass = require('gulp-sass');
+var prefix = require('gulp-autoprefixer');
+
+//server
+gulp.task('start', function() {
+  gulp.src('app')
+    .pipe(server({
+      livereload: true,
+      open: true
+    }));
+});
+
+
+//styles
+gulp.task('style', function () {
+  return gulp.src('app/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(prefix({
+        browsers:['last 15 versions']
+    }))
+    .pipe(gulp.dest('app/css'));
+});
+
+gulp.task('watch',function(){
+    gulp.watch('app/sass/**/*.scss',['style']);
+})
+
+gulp.task('default', ['start','watch']);
