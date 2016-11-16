6341815304c10265315cc5f22255a4eaa85108ff
Maxim Korovin <Maxim Korovin>
2016-11-16 17:51:53 +0300

    boxwood

--- app/js/controllers/chat.controller.js
+++
@@ -1,7 +0,0 @@
-function chatCtrl() {
-    var vm=this;
-
-    vm.test = 'TEST';
-}
-angular.module('cbsChat')
-.controller('chatCtrl',[chatCtrl]);
--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,19 @@ var gulp = require('gulp');
 var server = require('gulp-server-livereload');
 var sass = require('gulp-sass');
 var prefix = require('gulp-autoprefixer');
+var useref = require('gulp-useref');
+var gulpif = require('gulp-if');
+var uglify = require('gulp-uglify');
+var minifyCss = require('gulp-csso');
+var imagemin = require('gulp-imagemin');
+var cleanDest = require('gulp-clean-dest');
 
 //server
 gulp.task('start', function() {
   gulp.src('app')
     .pipe(server({
       livereload: true,
+      defaultFile: 'index.html',
       open: true
     }));
 });
@@ -23,8 +30,30 @@ gulp.task('style', function () {
     .pipe(gulp.dest('app/css'));
 });
 
+
+//IMAGES
+gulp.task('images', function () {
+    return gulp.src('./app/img/**/*')
+        .pipe(imagemin({
+          progressive: true
+        }))
+        .pipe(gulp.dest('public/img'));
+});
+
+
+//build
+gulp.task('build',['images'], function () {
+    return gulp.src('./app/*.html')
+        .pipe(cleanDest('public'))
+        .pipe(useref())
+        .pipe(gulpif('*.js', uglify()))
+        .pipe(gulpif('*.css', csso()))
+        .pipe(gulp.dest('public'));
+});
+
+
 gulp.task('watch',function(){
     gulp.watch('app/sass/**/*.scss',['style']);
-})
+});
 
 gulp.task('default', ['start','style','watch']);
